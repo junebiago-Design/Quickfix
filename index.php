6ec91bb52a480f8a78c30fbde9c3c9fda9794c82
junebiago-Design <junebiago@gmail.com>
2026-08-12 12:58:59 +0800

    css-cleanup
    remove unusable css tags codes in  index.min.css files

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $user = $_SESSION['user'];
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.5, user-scalable=yes" />
-    <title>Task Management System | TMS V1.0.7</title>
+    <title>Task Management System | TMS V1.0.8</title>
     <link rel="stylesheet" href="css/index.min.css" />
 </head>
 <body>
